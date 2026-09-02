4fa4f9cf080ed699ed49e66ba153d166d88ec516
kimseongyeon <seongyeon0810@naver.com>
2026-09-02 12:28:39 +0900

    feat: 세션 API 함수 작성

---
+++ src/api/session/session.ts
@@ -0,0 +1,35 @@
+import instance from "../instance";
+import type { ApiResponse } from "../../types/type";
+import type { SessionReviewResponse, SessionResponse, SessionDetailResponse  } from "../../types/session/session";
+
+export const editSessionReviews = async (commentId: number): Promise<ApiResponse<SessionReviewResponse[]>> => {
+    const response = await instance.put<ApiResponse<SessionReviewResponse[]>>(`/api/v1/sessions/comments/${commentId}`);
+    return response.data;
+}
+
+export const deleteSessionReview = async (commendId: number): Promise<ApiResponse<string>> => {
+    const response = await instance.delete<ApiResponse<string>>(`/api/v1/sessions/comments/${commendId}`);
+    return response.data;
+}
+
+export const getSessionReviews = async (sessionId: number): Promise<ApiResponse<SessionReviewResponse[]>> => {
+    const response = await instance.get<ApiResponse<SessionReviewResponse[]>>(`/api/v1/sessions/${sessionId}/comments`);
+    return response.data;
+}
+
+export const postSessionReview = async (sessionId: number, content: string): Promise<ApiResponse<SessionReviewResponse>> => {
+    const response = await instance.post<ApiResponse<SessionReviewResponse>>(`/api/v1/sessions/${sessionId}/comments`, { content });
+    return response.data;
+}
+
+export const getSessions = async (term: number, part: string): Promise<ApiResponse<SessionResponse>> => {
+    const response = await instance.get<ApiResponse<SessionResponse>>(`/api/v1/sessions`, {
+        params: { term, part }
+    });
+    return response.data;
+}
+
+export const getSessionDetail = async (sessionId: number): Promise<ApiResponse<SessionDetailResponse>> => {
+    const response = await instance.get<ApiResponse<SessionDetailResponse>>(`/api/v1/sessions/${sessionId}`);
+    return response.data;
+}   

9166c28679fbcb040d26bac166b1b08e8e1c337c
kimseongyeon <seongyeon0810@naver.com>
2026-09-09 01:13:07 +0900

    feat: SessionDetail 페이지 반응형 구현

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -22,30 +22,30 @@ export default function Modal({ open, title, message, onClose, onConfirm, confir
       onClick={onClose}
     >
       <div
-        className={`bg-white rounded-[20px] relative ${compact ? 'w-[400px] px-7 py-6' : 'w-[535px] px-10 py-9'}`}
+        className={`bg-white rounded-[20px] relative ${compact ? 'w-[315px] lg:w-[400px] px-7 py-6' : 'w-[315px] lg:w-[535px] px-10 py-9'}`}
         onClick={e => e.stopPropagation()}
       >
         <div className={`flex items-center justify-between ${compact ? 'mb-3' : 'mb-6'}`}>
-          <h2 className={compact ? 'text-[20px] font-semibold' : 'text-[32px] font-semibold'}>{title}</h2>
+          <h2 className={compact ? 'text-[20px] font-semibold' : 'text-[20px] lg:text-[32px] font-semibold'}>{title}</h2>
           <Button onClick={onClose} className="!p-0 w-6 h-6">
             <img src={DelIcon} alt="닫기" className="w-full h-full" />
           </Button>
         </div>
 
-        <p className={compact ? 'text-[15px] text-[#697584] mb-6' : 'text-[28px] text-[#697584] mb-6.25'}>{message}</p>
+        <p className={compact ? 'text-[15px] text-[#697584] mb-6' : 'text-4 lg:text-[28px] text-[#697584] mb-6.25'}>{message}</p>
 
-        <div className={`flex justify-end gap-4.25 ${compact ? '' : 'pt-22'}`}>
+        <div className={`flex gap-4.25 ${compact ? '' : 'pt-22'}`}>
           {!confirmOnly && (
             <Button
               onClick={onClose}
               color="white"
-              className={compact ? '!px-5 !py-2 !text-[14px]' : ''}>
+              className={`w-full ${compact ? '!px-5 !py-2 !text-[14px]' : ''}`}>
               취소
             </Button>
           )}
           <Button
             onClick={onConfirm}
-            className={`${confirmClassName ?? 'bg-red-500 text-white'} ${compact ? '!px-5 !py-2 !text-[14px]' : ''}`}>
+            className={`w-full ${confirmClassName ?? 'bg-red-500 text-white'} ${compact ? '!px-5 !py-2 !text-[14px]' : ''}`}>
             확인
           </Button>
         </div>

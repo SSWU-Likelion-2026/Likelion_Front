a97bbdf8c0fb49d112fdc76e20f522405b29c071
kimseongyeon <seongyeon0810@naver.com>
2026-09-12 13:36:50 +0900

    feat: 세션 폴더 글씨 말줌임표 추가

--- src/components/session/SessionFolder.tsx
+++ src/components/session/SessionFolder.tsx
@@ -44,9 +44,9 @@ export default function SessionFolder({ variant = 'default', part, week, title, 
           className={`absolute bottom-0 left-0 w-full transition-opacity duration-150 ${hovered ? 'opacity-100' : 'opacity-0'}`}
         />
       )}
-      <div className='flex flex-col absolute left-3 lg:left-6 right-3 lg:right-6 bottom-3 lg:bottom-5 gap-1'>
+      <div className='flex flex-col absolute left-3 lg:left-6 right-3 lg:right-6 bottom-3 lg:bottom-5 gap-1 overflow-hidden'>
         <p className='text-[11px] lg:text-[17px] text-white'>W{String(week).padStart(2, '0')}</p>
-        <p className='text-[13px] lg:text-[19px] font-semibold text-white'>{title}</p>
+        <p className='text-[13px] lg:text-[19px] font-semibold text-white truncate'>{title}</p>
       </div>
     </div>
   )

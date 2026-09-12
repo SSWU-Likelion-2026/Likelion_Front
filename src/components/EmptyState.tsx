6ed0d80720281fbc1d409eb2876b48ab075f17ed
kimseongyeon <seongyeon0810@naver.com>
2026-09-12 13:05:24 +0900

    feat: EmptyState 반응형 적용

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -5,7 +5,7 @@ interface Props {
 export default function EmptyState({ message }: Props) {
   return (
     <div className="flex min-h-[300px] w-full items-start justify-center pt-[20px]">
-      <p className="text-center text-[34px] font-semibold leading-[50px] text-black">{message}</p>
+      <p className="text-center text-[20px] lg:text-[34px] font-semibold leading-[50px] text-black">{message}</p>
     </div>
   )
 }
